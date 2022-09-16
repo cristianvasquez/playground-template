142a33ae78fcbd2fe0c1e68a8dc1e6c6882c0341
Cristian <cristianvasquez@gmail.com>
2022-09-16 16:47:23 +0200

    feat: expose default layout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Settings from './src/components/Settings.vue'
+import { defaultLayout } from './src/config.js'
 import Glayout from './src/layout/Glayout.vue'
 import { useLayoutStore } from './src/store/layout.js'
 
-export { Glayout, Settings, useLayoutStore }
+export { Glayout, Settings, useLayoutStore, defaultLayout }
