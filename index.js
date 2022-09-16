f317b064ba49394e7a68af5525cba783fff579c0
Cristian <cristianvasquez@gmail.com>
2022-09-16 16:31:41 +0200

    feat: add index

---
+++ index.js
@@ -0,0 +1,5 @@
+import Settings from './src/components/Settings.vue'
+import Glayout from './src/layout/Glayout.vue'
+import { useLayoutStore } from './src/store/layout.js'
+
+export { Glayout, Settings, useLayoutStore }
